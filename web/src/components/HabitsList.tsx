6ed40f1cabd69d37464b6a92053d333ec883140e
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2023-02-13 18:08:21 -0300

    Toggle for back-end the checks

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -30,6 +30,26 @@ export function HabitsList({ date }: HabitsListProps) {
         })
     }, [])
 
+    async function handleToggleHabit(habitId : string){
+        await api.patch(`habits/${habitId}/toggle`)
+
+        const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId)
+
+        let completedHabits: string[] = []
+
+        if(isHabitAlreadyCompleted){
+            //remover da lista
+            completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId)
+        } else {
+            //adicionar na lista
+            completedHabits: [...habitsInfo!.completedHabits, habitId]
+        }
+        setHabitsInfo({
+            possibleHabits: habitsInfo!.possibleHabits,
+            completedHabits,
+        })
+    }
+
     const isDateInPast = dayjs(date).endOf('day').isBefore(new Date())
 
     return (
@@ -38,6 +58,7 @@ export function HabitsList({ date }: HabitsListProps) {
                 return (
                     <Checkbox.Root
                         key={habit.id}
+                        onCheckedChange={() => handleToggleHabit(habit.id)}
                         checked={habitsInfo.completedHabits.includes(habit.id)}
                         disabled={isDateInPast}
                         className='flex items-center gap-3 group'
